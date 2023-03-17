347b785479041fc31a0f539922ec2983a06e463a
kosmo <kosmosensei@gmail.com>
2023-03-17 13:19:34 +0700

    complete hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+        String firstName  = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName   = "Ivanov";
+        String fullName    = lastName+" "+ firstName +" "+ middleName;
+        System.out.println(fullName);
+    }
+    private static void task2(){
+        System.out.println("Задача 2");
+    String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(fullName.toUpperCase());
+    }
+    private static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
